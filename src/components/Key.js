dc9c6ace28557aaa4e9d2dd1f2edb9c6c5095125
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-09 14:17:10 +0530

    Implement components

---
+++ src/components/Key.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Key.css'
+
+class Key extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Key</h1>
+            </div>
+        );
+    }
+}
+
+export {Key};
--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Key } from './Key';
+import './Piano.css'
 
 class Piano extends React.Component {
     render () {
         return (
-            <div>
-                <h1>Piano</h1>
+            <div className='piano'>
+                <Key />
+                <Key />
+                <Key />
+                <Key />
             </div>
         )
     }
